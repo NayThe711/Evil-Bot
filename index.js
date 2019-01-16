c23f47514ed8eb7a6ce149435b2e08aa5d4b0aaa
EvilDepictions <46579621+EvilDepictions@users.noreply.github.com>
2019-01-16 22:16:30 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+const Discord = require ("discord.js");
+const bot = new Discord.Client();
+const TOKEN = 'NTM0NDkyMTA3OTgxOTc5NjQ4.Dx_tPA.0N71Zrii11JtiP_4Sio7OXcAGpE'
+
+bot.on('message', function(message){
+    if(message.content == '£Hello')
+    {
+        message.reply('Hello I am EvilBot, how can I help?');
+    }
+});
+
+bot.on("guildMemberAdd", function(member)
+{
+    member.send("Welcome to Evil Clan");
+    let memberRole = member.guild.roles.find("name", "Member");
+    member.addRole(memberRole);
+});
+
+bot.on('message', function(message){
+
+    if(message.content == '£Claninfo')
+    {
+        message.reply('We are a sniping/modding clan. Homicide is the Founder of the clan and writer of me.');
+    }
+    if(message.content =='£Join')
+    {
+        message.member.send("Welcome to Evil Clan");
+    }
+
+});
+
+    bot.on('ready', function(){
+        console.log("Ready");
+    });
+
+
+
+client.login(process.env.BOT_TOKEN);
